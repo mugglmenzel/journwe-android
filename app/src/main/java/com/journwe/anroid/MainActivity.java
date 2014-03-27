69f26d747a60ba2cb4168e601eaf2e3bfe97a662
Max Becker <max.l.becker@web.de>
2014-03-27 22:34:03 +0100

    erste login-versuche

--- app/src/main/java/com/journwe/anroid/MainActivity.java
+++ app/src/main/java/com/journwe/anroid/MainActivity.java
@@ -1,10 +1,13 @@
 package com.journwe.anroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.EditText;
 
 import org.scribe.builder.ServiceBuilder;
@@ -45,26 +48,68 @@ public class MainActivity extends Activity {
 		EditText editPass = (EditText) findViewById(R.id.editPass);
 		String pass = editPass.getText().toString();
 
-        OAuthService service = new ServiceBuilder()
+        String callbackURL = "oauth://facebook";
+
+        final OAuthService s = new ServiceBuilder()
                 .provider(FacebookApi.class)
                 .apiKey("515340035175937")
-                .apiSecret("b5788518d689a6613a581722c98f4cf4")
+                .apiSecret("b5788518d689a6613a581722c98f4cf4").callback(callbackURL).debug().scope("email,xmpp_login")
                 .build();
 
         Log.i("login", "still running");
 
-		Token requestToken = service.getRequestToken();
-		String authUrl = service.getAuthorizationUrl(requestToken);
 
-		Verifier v = new Verifier("verifier you got from the user");
-		Token accessToken = service.getAccessToken(requestToken, v);
+		String authorisationURL = s.getAuthorizationUrl(null);
 
-		OAuthRequest request = new OAuthRequest(Verb.GET,
-				"https://graph.facebook.com/me");
-		service.signRequest(accessToken, request); // the access token from step
-													// 4
-		Response response = request.send();
-		editUser.setText(response.getBody());
+        final WebView webview = (WebView) findViewById(R.id.webview);
+      //  final Token requestToken = s.getRequestToken();
+        final String authURL = s.getAuthorizationUrl(null);
+
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+
+                //check for our custom callback protocol
+                //otherwise use default behavior
+                if(url.startsWith("oauth"))
+                {
+                    //authorization complete hide webview for now.
+                    webview.setVisibility(View.GONE);
+                    Uri uri = Uri.parse(url);
+                    String verifier = uri.getQueryParameter("oauth_verifier");
+                    Verifier v = new Verifier(verifier);
+
+                    //save this token for practical use.
+                    Token accessToken = s.getAccessToken(null, v);
+
+                    OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.fitbit.com/1/user/-/activities/date/2012-12-15.json");
+                    s.signRequest(accessToken, request);
+                    Response response = request.send();
+                    System.out.print("JSON:"+response.getCode());
+
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        //send user to authorization page
+        webview.loadUrl(authURL);
+
+        Log.i("login", authURL);
+
+//
+//		Verifier v = new Verifier("verifier you got from the user");
+//		Token accessToken = service.getAccessToken(requestToken, v);
+//
+//		OAuthRequest request = new OAuthRequest(Verb.GET,
+//				"https://graph.facebook.com/me");
+//		service.signRequest(accessToken, request); // the access token from step
+//													// 4
+//		Response response = request.send();
+//		editUser.setText(response.getBody());
     }
 
 }
