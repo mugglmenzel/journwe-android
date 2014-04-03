4a2450cd33325b1fdc67b557d06cd93591b639db
Max Becker <max.l.becker@web.de>
2014-04-03 16:44:03 +0200

    call versuch

--- src/com/journwe/android/JournWeActivity.java
+++ src/com/journwe/android/JournWeActivity.java
@@ -1,7 +1,7 @@
 package com.journwe.android;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.HttpURLConnection;
@@ -25,187 +25,191 @@ import android.widget.TextView;
 
 import com.facebook.Session;
 
-public class JournWeActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class JournWeActivity extends Activity implements
+		NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+	/**
+	 * Fragment managing the behaviors, interactions and presentation of the
+	 * navigation drawer.
+	 */
+	private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-    private Session s;
-    private static String userid;
-    private static String username;
+	/**
+	 * Used to store the last screen title. For use in
+	 * {@link #restoreActionBar()}.
+	 */
+	private CharSequence mTitle;
+	private Session session;
+	private static String userid;
+	private static String username;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_journ_we);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_journ_we);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+		mTitle = getTitle();
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-        
-        Intent intent = getIntent();
-        Bundle b = intent.getExtras();
-        
-        if (b != null) {
-        	s = (Session) b.get(MainActivity.SESSION);
-        	userid = (String) b.get(MainActivity.USER_ID);
-        	username = (String) b.get(MainActivity.USER_NAME);
-        }
-    }
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
+		Intent intent = getIntent();
+		Bundle b = intent.getExtras();
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
+		if (b != null) {
+			session = (Session) b.get(MainActivity.SESSION);
+			userid = (String) b.get(MainActivity.USER_ID);
+			username = (String) b.get(MainActivity.USER_NAME);
+		}
+		
+	}
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
+		// update the main content by replacing fragments
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager
+				.beginTransaction()
+				.replace(R.id.container,
+						PlaceholderFragment.newInstance(position + 1)).commit();
+	}
 
+	public void onSectionAttached(int number) {
+		switch (number) {
+		case 1:
+			mTitle = getString(R.string.title_section1);
+			break;
+		case 2:
+			mTitle = getString(R.string.title_section2);
+			break;
+		case 3:
+			mTitle = getString(R.string.title_section3);
+			break;
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.journ_we, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
+	public void restoreActionBar() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setTitle(mTitle);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			// Only show items in the action bar relevant to this screen
+			// if the drawer is not showing. Otherwise, let the drawer
+			// decide what to show in the action bar.
+			getMenuInflater().inflate(R.menu.journ_we, menu);
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    private static String call() {
-    	String re = "calling";
-    	String urlstring = "www.journwe.com/api/json/mobile/login";
-    	
-    	CookieManager cookieManager = new CookieManager();
-    	CookieHandler.setDefault(cookieManager);
-    	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private static String call() {
+		String re = "calling";
+		String urlstring = "www.journwe.com/api/json/mobile/login";
+
+		CookieManager cookieManager = new CookieManager();
+		CookieHandler.setDefault(cookieManager);
+
 		try {
 			Log.i("API call", urlstring);
 
-			URL url = new URL(urlstring.toString());
+			URL url = new URL(urlstring);
+
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			InputStream in = connection.getInputStream();
+			re = in.toString();
 			
-	    	try {
-				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-				OutputStream s = connection.getOutputStream();
-				re = s.toString();
-			} catch (IOException e) {
-				re = "IOException";
-				e.printStackTrace();
-			}
 		} catch (MalformedURLException e) {
 			re = "MalformedURLException";
 			e.printStackTrace();
+		} catch (IOException e) {
+			re = "IOException";
+			e.printStackTrace();
 		}
-//    	connection.setDoOutput(true);
-//    	java.net.CookieManager msCookieManager = new java.net.CookieManager();
-//    	msCookieManager.put(COOKIES_URI, connection.getHeaderFields());
-    	
-    	return re;
-    }
+		// connection.setDoOutput(true);
+		// java.net.CookieManager msCookieManager = new
+		// java.net.CookieManager();
+		// msCookieManager.put(COOKIES_URI, connection.getHeaderFields());
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+		return re;
+	}
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
+		/**
+		 * Returns a new instance of this fragment for the given section number.
+		 */
+		public static PlaceholderFragment newInstance(int sectionNumber) {
+			PlaceholderFragment fragment = new PlaceholderFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_journ_we, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            
-            String text = "";
-            
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
-            	text = username;
-            }
-            
-            else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
-            	text = userid;
-            }
-            
-            else if (getArguments().getInt(ARG_SECTION_NUMBER) == 3) {
-            	text = call();
-            }
-            
-            textView.setText(text); //Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
+		public PlaceholderFragment() {
+		}
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((JournWeActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_journ_we,
+					container, false);
+			TextView textView = (TextView) rootView
+					.findViewById(R.id.section_label);
+
+			String text = "";
+
+			if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+				text = username;
+			}
+
+			else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
+				text = userid;
+			}
+
+			else if (getArguments().getInt(ARG_SECTION_NUMBER) == 3) {
+				text = call();
+			}
+
+			textView.setText(text); // Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+			return rootView;
+		}
+
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			((JournWeActivity) activity).onSectionAttached(getArguments()
+					.getInt(ARG_SECTION_NUMBER));
+		}
+	}
 }
